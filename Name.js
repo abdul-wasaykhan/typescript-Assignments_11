02fc0acee660537b0068ab2630e5711a1ab33e9b
Abdul Wasay Khan <160697983+abdul-wasaykhan@users.noreply.github.com>
2024-02-21 11:38:15 +0500

    Add files via upload

---
+++ Name.js
@@ -0,0 +1,5 @@
+"use strict";
+let names = ['Talha', 'Arif', 'Asif', 'Taha'];
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
---
+++ Name.ts
@@ -0,0 +1,9 @@
+
+
+let names :string[] = ['Talha','Arif','Asif','Taha'] ;
+
+for(let i=0; i<names.length; i++){
+
+    console.log(names[i]) ;
+
+}
